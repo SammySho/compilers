2f6414e5ea22a87fee527ec4824b1b6c701c6a44
SammySho <45356772+SammySho@users.noreply.github.com>
2018-12-11 15:57:26 +0000

    Create Task1.java

---
+++ Task1.java
@@ -0,0 +1,13 @@
+public class Task1 {
+// Do not modify the code below except for replacing the "..."!  Don't
+// add anything (including "public" declarations), don't remove
+// anything (including "public" declarations). Don't wrap it in a
+// package, don't make it an innner class of some other class.  If
+// your IDE suggsts to change anything below, ignore your IDE. You are
+// welcome to add new classes! Please put them into separate files.
+
+        public static Codegen create () throws basic.CodegenException {
+            return new Codegen();
+        }
+}
+
